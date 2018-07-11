f080a763db62fea209ffd754235870bb91c73fc6
DezkaReid <elmaildeldezkareid@gmail.com>
2018-07-10 20:26:13 -0500

    Optimización para producción

--- lazy-loading/webpack.config.js
+++ lazy-loading/webpack.config.js
@@ -6,6 +6,17 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const config = (env, args) => {
   const commonConfig = common(env, args)
   return merge(commonConfig, {
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          commons: {
+            name: 'commons',
+            chunks: 'all',
+            minChunks: 2
+          }
+        }
+      }
+    },
     plugins: [
       new MiniCssExtractPlugin({
         filename: '[name].css'
