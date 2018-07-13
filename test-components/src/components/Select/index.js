1f7161c5a7defb283c142b079cfde9399bb5e767
DezkaReid <elmaildeldezkareid@gmail.com>
2018-07-13 16:44:07 -0500

    added support to add onchange event and value property

--- test-components/src/components/Select/index.js
+++ test-components/src/components/Select/index.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
 export default function Select (props) {
-  const { items = [] } = props
+  const {
+    items = [],
+    defaultOption = 'Select a option'
+  } = props
   return (
     <select {...props}>
+      <option>{defaultOption}</option>
       {items.map(item => <option key={item}>{item}</option>)}
     </select>
   )
--- test-components/tests/components/Select/Select-test.js
+++ test-components/tests/components/Select/Select-test.js
@@ -3,9 +3,35 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import Select from '../../../src/components/Select'
 describe('Select component test suite', () => {
-  test('The select should have two elements options when recieves two items', () => {
+  test('The select should have 3 children when recieves two items', () => {
     const items = ['super mario world', 'megaman x']
     const element = shallow(<Select items={items} />)
-    expect(element.find('select').children()).toHaveLength(2)
+    expect(element.find('select').children()).toHaveLength(3)
+  })
+
+  test('The default option can be specified or not', () => {
+    const wrapperElement = shallow(<Select />)
+    let defaultOption = wrapperElement.find('select').childAt(0)
+    expect(defaultOption.text()).toEqual('Select a option')
+
+    const customOptionMessage = 'Choose one'
+    wrapperElement.setProps({ defaultOption: customOptionMessage })
+    defaultOption = wrapperElement.find('select').childAt(0)
+    expect(defaultOption.text()).toEqual(customOptionMessage)
+  })
+
+  test('Should call a function when selected option has changed', () => {
+    const spyOn = jest.fn()
+    const wrapperElement = shallow(<Select onChange={spyOn} />)
+    wrapperElement.find('select').simulate('change')
+    expect(spyOn).toBeCalled()
+  })
+
+  test('The if value is provided must be match with option selected', () => {
+    const items = ['super mario world', 'megaman x']
+    const valueSelected = 'megaman x'
+    const wrapperElement = shallow(<Select items={items} value={valueSelected} />)
+    const select = wrapperElement.find('select')
+    expect(select.prop('value')).toEqual(valueSelected)
   })
 })
