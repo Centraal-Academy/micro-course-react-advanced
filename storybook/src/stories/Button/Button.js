18c028f2a6cb9c06ec97c8eaddf6b0f13dadbb38
DezkaReid <elmaildeldezkareid@gmail.com>
2018-10-15 19:28:52 -0500

    Added states and tests

--- storybook/src/stories/Button/Button.js
+++ storybook/src/stories/Button/Button.js
@@ -2,14 +2,15 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 import { withNotes } from '@storybook/addon-notes'
+import { linkTo } from '@storybook/addon-links'
 import Button from '../../components/Button'
 import withTests from './withTest'
 
 storiesOf('Button', module)
   .addDecorator(withTests('Button'))
-  .add('with text', () => (
-    <Button onClick={action('clicked')}>Hello Button</Button>
+  .add('Simple button', () => (
+    <Button onClick={linkTo('Button', 'Button with notes')}>Go Button Two</Button>
   ))
   .add('Button with notes',
-    withNotes('Sample text')(() => <Button onClick={action('clicked')}>Hello Button</Button>)
+    withNotes('Sample text')(() => <Button onClick={action('clicked')}>Button Two</Button>)
   )
